d79c3b4e55b27dc22d5cd68b2c86fa2241b3ff16
Ronald5607 <roni.paloniemi@hotmail.com>
2022-09-26 22:54:47 +0300

    jotain

--- SQL.py
+++ SQL.py
@@ -1,42 +1,77 @@
 import mysql.connector
 
+
+class Column:
+    def __init__(self, name):
+        self.owner = None
+        self.name = name
+
+    def __get__(self, instance, owner):
+        if instance:
+            self.owner = instance
+        return self
+
+    def __str__(self):
+        if self.owner:
+            return self.owner.name + '.' + self.name
+        else:
+            return self.name
+
+    def __add__(self, other):
+        return self.name + other
+
+    def __radd__(self, other):
+        return other + self.name
+
+    def __lt__(self, other):
+        return self.name + '<' + other
+
+    def __eq__(self, other):
+        return self.name + '=' + other
+
+
 class Table:
-    search = ('*',)
+    search = (Column('*'),)
 
     def __init__(self, *args):
         self.search = ()
+        temp = []
         for arg in args:
             if not hasattr(self, arg):
                 raise Exception(f'ei ole saraketta {arg}')
-            self.search = args
+            else:
+                temp.append(Column(arg))
+            self.search = tuple(temp)
+
+
 
 
 class Airport(Table):
-    name = 'airport'
-    ident = 'ident'
-    type = 'type'
-    latitude = 'latitude_deg'
-    longitude = 'longitude_deg'
-    elevation = 'elevation_ft'
-    continent = 'continent'
-    country = 'iso_country'
+    name = Column('airport')
+    ident = Column('ident')
+    type = Column('type')
+    latitude = Column('latitude_deg')
+    longitude = Column('longitude_deg')
+    elevation = Column('elevation_ft')
+    continent = Column('continent')
+    country = Column('iso_country')
 
 
 class Country(Table):
-    name = 'country'
-    country = 'iso_country'
+    name = Column('country')
+    country = Column('iso_country')
 
 
 class Yhteys:
     def __init__(self, salasana):
-        yhteys = mysql.connector.connect(
+        self.yhteys = mysql.connector.connect(
             host='127.0.0.1',
             port=3306,
             database='flight_game',
             user='root',
             password=salasana,
             autocommit=True)
-        self.cursor = yhteys.cursor()
+        self.cursor = self.yhteys.cursor()
 
     def _exec(self, SQLstring):
         self.cursor.execute(SQLstring.payload)
@@ -55,7 +90,11 @@ class SQLstring:
     def where(self, *args):
         self.payload += ' WHERE '
         for arg in args:
-            self.payload += str(arg)
+            self.payload += "'" + str(arg) + "'"
+        return self
+
+    def limit(self, limit):
+        self.payload += ' LIMIT ' + str(limit)
         return self
 
 
@@ -65,7 +104,7 @@ class Select(SQLstring):
         frompayload = ' FROM '
         for table in args:
             for column in table.search:
-                self.payload += str(table.name + '.' + column) + ', '
+                self.payload += str(table.name) + '.' + str(column) + ', '
 
             frompayload += str(table.name)
             frompayload += ', '
@@ -75,5 +114,12 @@ class Select(SQLstring):
 
 
 if __name__ == '__main__':
-    aa = Select(Airport('ident', 'name'), Country('name')).where('1=1')
+    # sql = Yhteys('roni')
+    airport = Airport('ident', 'name')
+    country = Country('name')
+    print(country.name)
+    aa = (Select(airport, country)
+            .where(country.country == airport.country)
+            .limit(10))
+    # print(sql.hae(aa, monta=True))
     print(aa.payload)
