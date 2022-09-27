186444e2d4db08bdb5173975cf1542d2eb235b38
Ronald5607 <roni.paloniemi@hotmail.com>
2022-09-27 17:32:32 +0300

    jep

--- screen.py
+++ screen.py
@@ -1,31 +1,47 @@
+import shutil
 import os
 
 
 class Screen:
     # hoitaa terminaalille tulostamisen.
+    # __init__() kutsutaan kun luodaan objekti Screen.
     def __init__(self):
-        self._size = 0
+        self._size = shutil.get_terminal_size()
+        self.width, self.height = self._size
 
+# @property dekoraattori tarkoittaa että metodin size voi kutsua ilman sulkuja "Screen.size"
     @property
     def size(self):
         return self._size
 
+# hakee terminaalin koon.
     def get_size(self):
-        self._size = os.get_terminal_size()
+        self._size = shutil.get_terminal_size()
+        self.width, self.height = self._size
 
-    def clear(self):
+# @staticmethod ei tarvitse objektia parametrinä, voi kutsua ilman objektia. metodi pyyhkii terminaali ruudun.
+    @staticmethod
+    def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
 
     def set_prompt(self):
         ...
 
+    @staticmethod
+    def emptyline():
+        print('/n')
 
 
 if __name__ == '__main__':
-    print('a')
+
+    print(os.name)
     a = Screen()
-    a.set_prompt()
-    input('a')
-    print('b')
+    a.get_size()
+    a.clear()
+    print(a.size)
+    input('c')
+    a.clear()
+    a.get_size()
+    print(a.size)
     input('b')
 
