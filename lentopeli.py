91299f83a7f683817a91e1c59d7846c1872c49a2
Ronald5607 <roni.paloniemi@hotmail.com>
2022-09-29 18:04:41 +0300

    voi piirtää lentokoneen :D

--- lentopeli.py
+++ lentopeli.py
@@ -9,10 +9,12 @@ havinnyt = False
 
 while not havinnyt:
 
+    ikkuna.get_size()
     ikkuna.clear()
     ikkuna.draw_text_box(ikkuna.top_right[0] - 13, ikkuna.top_right[1], 'Pisteet:', str(5003))
     ikkuna.draw_airplane(ikkuna.center[0], ikkuna.center[1])
     ikkuna.flush()
+
 
     komento = input('mitä teet: ')
     if komento == 'lopeta':
