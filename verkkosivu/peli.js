9b00898603f19697ccdd34b2a1a17f356ff32a44
Ronald5607 <roni.paloniemi@hotmail.com>
2022-12-06 18:59:58 +0200

    a

--- verkkosivu/peli.js
+++ verkkosivu/peli.js
@@ -1,6 +1,6 @@
-const map = L.map('map').setView([62.2426, 25.7473], 8);
+const map = L.map('map').setView([62.2426, 25.7473], 7);
 
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+const tiles = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 }).addTo(map);
