f46316499d46aa296685751bb7eae74dec7696d5
joromm <joromm2@gmail.com>
2022-12-07 16:07:44 +0200

    epic animatio index omg

--- verkkosivu/index.js
+++ verkkosivu/index.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const btn = document.querySelector('.ei')
+const btn = document.querySelector('.ei');
 btn.addEventListener('click', function() {
-  alert('nössö')
+  alert('nössö');
 });
+
+let id = null;
+const elem = document.querySelector('.animation');
+let pos = -300;
+clearInterval(id);
+id = setInterval(frame, 5);
+function frame() {
+  if (pos === 3000) {
+    clearInterval(id);
+  } else {
+    pos++;
+    elem.style.left = pos + 'px';
+  }
+}
