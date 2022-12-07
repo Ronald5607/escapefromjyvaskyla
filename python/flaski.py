7b75ed7c65c25fb688e36e46bb5dbd249b84a97b
Maria Weckström <maria.weckstrom@metropolia.fi>
2022-12-07 17:05:15 +0200

    nimi haku

--- python/flaski.py
+++ python/flaski.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, request
 from flask_cors import CORS
 from database import Database
 import requests
@@ -6,12 +6,13 @@ import json
 
 app = Flask(__name__)
 cors = CORS(app)
-db = Database()
+db = Database() #testausta varten check out database.py
 
+
+@app.route('/weather/<icao>')
 #hakee sään icao koodin perusteella
 #linkki: http://127.0.0.1:3000/weather/(icao) -> palauttaa pilvisyyden määrän
 #for now laitoin databaseen muistiin myös api keyn. idk
-@app.route('/weather/<icao>')
 def weather(icao):
     sql = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident='" + icao + "'"
     kursori = db.get_connection().cursor()
@@ -21,24 +22,35 @@ def weather(icao):
         vastaus = "ICAO-koodi on syötetty väärin."
     else:
         lat = tulos[0]
-        print(lat)
+        #print(lat)
         lon = tulos[1]
-        print(lon)
+        #print(lon)
         weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={db.apikey}").json()
         vastaus = {
             "cloudiness": weather["clouds"]["all"]
         }
     return vastaus
 
-# @app.errorhandler(404)
-# def page_not_found(virhekoodi):
-#     vastaus={
-#         "viesti": "Päätepistettä ei löydy",
-#         "status": 404
-#     }
-#     json_vastaus=json.dumps(vastaus)
-#     http_vastaus=Response(response=json_vastaus, status=404, mimetype="application/json")
-#     return http_vastaus
+@app.route("/name", methods=["POST"])
+#hakee index sivulta javascriptillä lähetetyn pelaajan nimen
+#ei hyväksyny ku koitin pyörittää tätä laittamal app.runiin localhostin mut ilman hostin määritystä toimii
+def get_name():
+    dict_name = request.get_json()
+    player_name = dict_name["name"]
+    print(player_name) #tässä pitäis tulostua pythoniin pelaajan nimi
+    #tässä voi tehdä nyt jotain sillä nimellä niiku vaik inserttaa se databaseen
+    return player_name
+
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus={
+        "viesti": "Päätepistettä ei löydy",
+        "status": 404
+    }
+    json_vastaus=json.dumps(vastaus)
+    http_vastaus=Response(response=json_vastaus, status=404, mimetype="application/json")
+    return http_vastaus
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    app.run(port=3000)
--- verkkosivu/index.js
+++ verkkosivu/index.js
@@ -18,3 +18,30 @@ function frame() {
     elem.style.left = pos + 'px';
   }
 }
+
+// lähettää formiin laitetun nimen json pakettina pythonille
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  const name = document.querySelector('input[type=text]');
+  const dict_name = {"name": name.value};
+  alert(name.value);
+  fetch("http://127.0.0.1:3000/name",
+  {
+  method: 'POST',
+  headers: {
+  'Content-type': 'application/json',
+  'Accept': 'application/json'
+  },
+  body:JSON.stringify(dict_name)}).then(res=>{
+  if(res.ok){
+  return res.json()
+  }else{
+  alert("something is wrong")
+  }
+  }).then(jsonResponse=>{
+  console.log(jsonResponse)
+  }
+  ).catch((err) => console.error(err));
+})
+
