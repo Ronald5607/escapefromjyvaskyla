7e133e90aeac403410ac0e7991d57bff6d742e4f
Ronald5607 <roni.paloniemi@hotmail.com>
2022-12-07 15:20:22 +0200

    j

--- verkkosivu/js/peli.js
+++ verkkosivu/js/peli.js
@@ -15,7 +15,6 @@ const img = document.createElement('img');
 
 
 img.addEventListener("load", () => {
-    console.log('loaded');
     ctx.drawImage(img, 0, 0);
 });
 img.addEventListener('error', (err) => {
@@ -23,7 +22,99 @@ img.addEventListener('error', (err) => {
 });
 img.setAttribute('src', 'js/lentsikka.png');
 
-if (img.complete) {
-    console.log('joo');
-    ctx.drawImage(img, 0, 0)
+
+let center = map.getCenter();
+let pixelorigin = map.getPixelOrigin();
+
+let pxcenter = map.project(center);
+
+console.log(center);
+console.log(pixelorigin);
+const x = pxcenter.x - pixelorigin.x
+console.log(x);
+ctx.clearRect(0, 0, 1000, 1000);
+ctx.translate(x, 0);
+
+
+function lentokentta_layer(lentokentat) {
+    const lentokenttalayer = [];
+    const punainen = new L.Icon({
+        iconUrl: 'punainenmarker.png',
+        iconSize: [20, 20],
+        iconAnchor: [10, 10]
+    });
+    for (let lentokentta of lentokentat) {
+        const markkeri = L.marker([parseFloat(lentokentta.latitude), parseFloat(lentokentta.longitude)], {
+            icon:punainen
+        });
+        lentokenttalayer.push(markkeri);
+    }
+    return L.layerGroup(lentokenttalayer);
 };
+
+function kaydyt_lentokentat(sijainti, kaydyt) {
+    const kentat = kaydyt.getLayers();
+    if (kentat.length > 0 && kentat.length > 9) kentat.shift();
+    kentat.push(L.marker([sijainti.latitude, sijainti.longitude]));
+    return L.layerGroup(kentat);
+
+}
+
+function piirra_lentokentat(lentokenttalayer, kaydyt) {
+
+}
+
+const lentokentat = [
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    }
+]
+let a = [
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    },
+    {
+        latitude: 62.2426,
+        longitude: 25.7473
+    }
+]
+const layeri = lentokentta_layer(a);
+let kaydyt = layeri;
+layeri.addTo(map);
+
+kaydyt = kaydyt_lentokentat({latitude: 55, longitude: 43}, kaydyt);
+console.log(kaydyt.getLayers());
