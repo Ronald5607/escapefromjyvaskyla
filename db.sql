baaae029d470001fa15b2aa2bbbbebb670b7c024
elias <elias.e.kyllonen@gmail.com>
2022-12-09 13:58:58 +0200

    viime kentät table+auto increment id

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ DROP TABLE goal_reached;
 SET FOREIGN_KEY_CHECKS=1;
  
 CREATE TABLE player (
-ID int not null,
+ID int not null auto_increment,
 screen_name varchar(20),
 points int,
 hp int,
@@ -14,7 +14,7 @@ primary key (ID)
 );
  
 CREATE TABLE enemy (
-enemy_ID int not null,
+enemy_ID int not null auto_increment,
 e1 varchar(20),
 e2 varchar(20),
 e3 varchar(20),
@@ -27,4 +27,20 @@ e4_moves int,
 e5_moves int,
 primary key (enemy_ID),
 foreign key (enemy_ID) REFERENCES player(ID)
+);
+
+CREATE TABLE last_airports (
+ID int not null auto_increment,
+last1 varchar(10),
+last2 varchar(10),
+last3 varchar(10),
+last4 varchar(10),
+last5 varchar(10),
+last6 varchar(10),
+last7 varchar(10),
+last8 varchar(10),
+last9 varchar(10),
+last10 varchar(10),
+primary key (ID),
+foreign key (ID) REFERENCES player(ID)
 );
