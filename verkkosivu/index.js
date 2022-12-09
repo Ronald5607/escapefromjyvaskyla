99b3247462ba8968b0ef92711e1f7e17a173adc6
joromm <joromm2@gmail.com>
2022-12-09 13:24:45 +0200

    animatio fix epic

--- verkkosivu/index.js
+++ verkkosivu/index.js
@@ -7,12 +7,13 @@ btn.addEventListener('click', function() {
 
 let id = null;
 const elem = document.querySelector('.animation');
-let pos = -300;
+let pos = -200;
 clearInterval(id);
-id = setInterval(frame, 5);
+id = setInterval(frame, 1);
+
 function frame() {
-  if (pos === 3000) {
-    clearInterval(id);
+  if (pos === 2180) {
+    pos = -200;
   } else {
     pos++;
     elem.style.left = pos + 'px';
@@ -24,24 +25,25 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', function(event) {
   const name = document.querySelector('input[type=text]');
-  const dict_name = {"name": name.value};
+  const dict_name = {'name': name.value};
   alert(name.value);
-  fetch("http://127.0.0.1:3000/name",
-  {
-  method: 'POST',
-  headers: {
-  'Content-type': 'application/json',
-  'Accept': 'application/json'
-  },
-  body:JSON.stringify(dict_name)}).then(res=>{
-  if(res.ok){
-  return res.json()
-  }else{
-  alert("something is wrong")
-  }
-  }).then(jsonResponse=>{
-  console.log(jsonResponse)
-  }
+  fetch('http://127.0.0.1:3000/name',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify(dict_name),
+      }).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      alert('something is wrong');
+    }
+  }).then(jsonResponse => {
+        console.log(jsonResponse);
+      },
   ).catch((err) => console.error(err));
-})
+});
 
