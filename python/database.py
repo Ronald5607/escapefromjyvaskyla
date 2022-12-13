5fbbb31e4fa9553c45dafd692c8863dd9613d0a5
joromm <joromm2@gmail.com>
2022-12-13 13:59:47 +0200

    epic jyväskyl ä airport picture

--- python/database.py
+++ python/database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 
 apikey = "cd6c905dd4cd2cdaf0b0f583cb078ec7" #testausta varten voi lisätä tähän apikeyn
-salasana = "roni" #testausta varten voi lisätä tähän salasanan
+salasana = "juurisalis" #testausta varten voi lisätä tähän salasanan
 tietokanta = 'flight_game'
 käyttäjä = 'root'
 
